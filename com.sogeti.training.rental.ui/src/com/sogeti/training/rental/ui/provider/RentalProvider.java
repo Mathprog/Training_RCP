cbb0f15b567a2e8ae7c57a44ec8df89c34dcb694
mathimar <mathimar@LFR031046.corp.capgemini.com>
2019-01-30 14:32:53 +0100

    Affichage des Nodes d'une agence.

--- com.sogeti.training.rental.ui/src/com/sogeti/training/rental/ui/provider/RentalProvider.java
+++ com.sogeti.training.rental.ui/src/com/sogeti/training/rental/ui/provider/RentalProvider.java
@@ -6,6 +6,7 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 
 import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Device;
 import com.opcoach.training.rental.RentalAgency;
 
 public class RentalProvider extends LabelProvider implements ITreeContentProvider {
@@ -27,7 +28,10 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		Object[] result = null;
 		
 		if (parentElement instanceof RentalAgency) {
-			result = ((RentalAgency) parentElement).getCustomers().toArray();
+			RentalAgency a = (RentalAgency) parentElement;
+			result = new Node[] {new Node(Node.CUSTOMERS, a), new Node(Node.RENTALS, a), new Node(Node.OBJETS_A_LOUER, a)};
+		} else if (parentElement instanceof Node) {
+			result = ((Node) parentElement).getChildren();
 		}
 		
 		return result;
@@ -35,13 +39,28 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 
 	@Override
 	public Object getParent(Object element) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public boolean hasChildren(Object element) {
-		return true;
+		/*if(element instanceof RentalAgency) {
+			RentalAgency rentalAgency = (RentalAgency) element;
+			if(rentalAgency.getCustomers().size() != 0 || rentalAgency.getObjectsToRent().size() != 0 || rentalAgency.getRentals().size() != 0) {
+				return true;
+			}
+		} else if (element instanceof Node) {
+			Node node = (Node) element;
+			if(node.getChildren().length != 0) {
+				return true;
+			}
+		}*/
+		
+		if(element instanceof RentalAgency || element instanceof Node) {
+			return true;
+		}
+		return false;
 	}
 	
 	public String getText(Object element) {
@@ -49,10 +68,53 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 			return ((RentalAgency) element).getName();
 		} else if (element instanceof Customer) {
 			return ((Customer) element).getDisplayName();
+		} else if (element instanceof Device) {
+			return ((Device) element).getName();
 		}
 		else {
 			return super.getText(element);
 		}
+	}
+	
+	private class Node {
+		public static final String OBJETS_A_LOUER = "Objets à louer";
+		public static final String RENTALS = "Rentals";
+		public static final String CUSTOMERS = "Customers";
+		private String label;
+		private RentalAgency rentalAgency;
+		
+		public Node (String label, RentalAgency rentalAngency) {
+			this.label = label;
+			this.rentalAgency = rentalAngency;
+		}
+		
+		@Override
+		public String toString() {
+		
+			return label;
+		}
+		
+		public Object[] getChildren() {
+			if(label == CUSTOMERS) {
+				return rentalAgency.getCustomers().toArray();
+			} else if (label == RENTALS) {
+				return rentalAgency.getRentals().toArray();
+			} else if (label == OBJETS_A_LOUER) {
+				return rentalAgency.getObjectsToRent().toArray();
+			} else {
+				return null;
+			}
+		}
+
+		public String getLabel() {
+			return label;
+		}
+
+		public void setLabel(String label) {
+			this.label = label;
+		}
+		
+		
 	}
 
 }
