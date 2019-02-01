4bef0999faf7261631c77df728cd7e10dda16f8b
mathimar <mathimar@LFR031046.corp.capgemini.com>
2019-02-01 10:46:50 +0100

    Ajout des menus

--- com.sogeti.training.rental.rcp/src/com/sogeti/training/rental/rcp/ApplicationActionBarAdvisor.java
+++ com.sogeti.training.rental.rcp/src/com/sogeti/training/rental/rcp/ApplicationActionBarAdvisor.java
@@ -5,8 +5,14 @@ import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.actions.ActionFactory;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+	private IAction quitAction;
+	private IAction quitAction_1;
+	private IAction preferencesAction;
 
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
@@ -15,11 +21,33 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	@Override
 	protected void makeActions(IWorkbenchWindow window) {
 		super.makeActions(window);
+		{
+			quitAction = ActionFactory.QUIT.create(window);
+			register(quitAction);
+		}
+		{
+			quitAction_1 = ActionFactory.QUIT.create(window);
+			register(quitAction_1);
+		}
+		{
+			preferencesAction = ActionFactory.PREFERENCES.create(window);
+			register(preferencesAction);
+		}
 	}
 	
 	@Override
 	protected void fillMenuBar(IMenuManager menuBar) {
 		super.fillMenuBar(menuBar);
+		
+		MenuManager menuManager = new MenuManager("New MenuManager");
+		menuManager.setMenuText("File");
+		menuBar.add(menuManager);
+		menuManager.add(quitAction_1);
+		
+		MenuManager menuManager_1 = new MenuManager("New MenuManager");
+		menuManager_1.setMenuText("Window");
+		menuBar.add(menuManager_1);
+		menuManager_1.add(preferencesAction);
 	}
 	
 	@Override
